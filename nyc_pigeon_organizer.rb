7e9e5683be05cc82c4257715c98c64dc1100ccff
Lauren Frankovich <frankovister@gmail.com>
2020-05-28 01:44:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = data.each_with_object({}) do |(key, value), result|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !result[name]
+          result[name] = {}
+        end
+        if !result[name][key]
+          !result[name][key] = []
+        end
+        result[name][key].push(inner_key.to_s)
+      end
+    end  
+  end
 end
+ 
+ 
